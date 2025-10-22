f72986b57f8fb613535a23c09604f59094a370f8
iPenguwin <220711663@students.uajy.ac.id>
2025-10-22 11:58:38 +0700

    update

--- types/types.go
+++ types/types.go
@@ -20,6 +20,6 @@ type User struct {
 type RegisterUserPayload struct {
 	FirstName string `json:"firstName" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
-	Email     string `json:"email" validate:"required"`
-	Password  string `json:"password" validate:"required, min=3, max=130"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=3,max=130"`
 }
